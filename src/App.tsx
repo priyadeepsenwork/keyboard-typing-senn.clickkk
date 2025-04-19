65fef4512b6a157886d062f60750655e52819326
priyadeepsenwork <work.priyadeepsen@gmail.com>
2025-04-19 14:31:08 +0530

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {faker} from '@faker-js/faker'
+
+const App = () => {
+ 
+  return (
+    <>
+      <h1 className="text-4xl text-[#ff004f]">Senn.Clicckk</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+const colors = require('tailwindcss/colors')
+
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.white,
+      },
+    },
+  },
+}
